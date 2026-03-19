6ab94a08907813c6258068cac80249ff541fafaf
vidhyasagar <vs1522250@gmail.com>
2026-03-19 16:22:45 +0530

    insta

---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv")
+const app = require("./src/app");
+const connectToDb = require("./src/config/Databse");
+
+connectToDb();
+
+app.listen(3000, () => {
+  console.log("server is running on port number 3000");
+});
